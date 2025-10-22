18fc145681b30b5324a275e45daada84c732c361
yusukensanta <yusuke.nakayama1218@gmail.com>
2025-10-22 23:10:20 +0900

    feat: add brew tap ruby script

---
+++ Formula/parqueteer.rb
@@ -0,0 +1,31 @@
+class Parqueteer < Formula
+  desc "CLI tool for working with Parquet files - query, inspect, and convert with ease"
+  homepage "https://github.com/yusukensanta/parqueteer"
+  version "0.1.0"
+  url "https://github.com/yusukensanta/parqueteer/releases/download/v0.1.0/parqueteer-0.1.0.tgz"
+  sha256 "" # Will be calculated during release
+  license "Apache-2.0"
+
+  # Requires Java 21 or later
+  depends_on "openjdk@21"
+
+  def install
+    # Remove Windows batch files
+    rm_f Dir["bin/*.bat"]
+
+    # Install all files to libexec
+    libexec.install Dir["*"]
+
+    # Create symlink for the main binary
+    bin.install_symlink libexec/"bin/parqueteer"
+
+    # Set JAVA_HOME to openjdk@21
+    bin.env_script_all_files libexec/"bin", JAVA_HOME: Formula["openjdk@21"].opt_prefix
+  end
+
+  test do
+    # Test that the binary exists and runs
+    output = shell_output("#{bin}/parqueteer --version 2>&1")
+    assert_match version.to_s, output
+  end
+end
